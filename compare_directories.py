501575056573980e29269523894ea983428485fd
IvanStamatov <ivan.t.stamatov@gmail.com>
2025-08-18 23:55:41 +0300

    T004 - Adding a check if the input is local or remote dir

--- compare_directories.py
+++ compare_directories.py
@@ -9,33 +9,55 @@
 
 
 import requests # Needed to check if the remote repository is accessible
+import os # Needed to navigate/check local directories
 
 def validate_input(repo_url):
     if not repo_url:
         print("Provided value for repository is empty ")
         return False
 
-    # Check if the URL is accessible
-    try:
-        response = requests.head(repo_url)
-        if response.status_code != 200:
-            print(f"[Error] Remote repository: [{repo_url}] is not accessible")
+    # Check if the input is a local directory or a URL
+    if repo_url.startswith("http://") or repo_url.startswith("https://"):
+        print(f"[Info] Detected remote repository URL: [{repo_url}]")
+        # Check if the URL is accessible
+        try:
+            response = requests.head(repo_url)
+            if response.status_code != 200:
+                print(f"[Error] Remote repository: [{repo_url}] is not accessible")
+                return False
+            else:
+                print(f"[Success] Remote repository: [{repo_url}] is accessible")
+        except requests.RequestException as e:
+            print(f"[Error] Invalid Repository URL: [{repo_url}]")
+            return False
+    else:
+        print(f"[Info] Detected local directory: [{repo_url}]")
+        # Check if the local directory exists
+        if not os.path.isdir(repo_url):
+            print(f"[Error] Local directory: [{repo_url}] does not exist")
             return False
         else:
-            print(f"[Success] Remote repository: [{repo_url}] is accessible")
-    except requests.RequestException as e:
-        print(f"[Error] Invalid Repository URL: [{repo_url}]")
-        return False
-    
+            print(f"[Success] Local directory: [{repo_url}] is valid")
+
     # If there are no errors in the checks above, return that the input is valid
     return True
 
 def main():
+    # Tested with local paths as well
     repo_url = "https://github.com/IvanStamatov/ll-challenge"
-    if validate_input(repo_url):
-        print("[Success] Repository URL is valid")
-    else:
-        print("[Error] Repository URL is invalid")
+
+    # Temporary section, input might be a list
+    directory_1 = "https://github.com/IvanStamatov/ll-challenge"
+    directory_2 = "https://github.com/IvanStamatov/ll-challenge"
+
+    # Make a list from the two variables
+    directories = [directory_1, directory_2]
+
+    for directory in directories:
+        if validate_input(directory):
+            print(f"[Success] Directory URL is valid: {directory}")
+        else:
+            print(f"[Error] Directory URL is invalid: {directory}")
 
 if __name__ == "__main__":
     main()
